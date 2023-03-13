b114815cae7f16174f3f53b637ee6b08d9daeac4
ArtemKuzyk <artem.brar@gmail.com>
2023-03-13 21:43:02 +0200

    add hash roter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from 'react';
-// import { CharactersProvider } from './hooks/useCharacters';
-import { CharacterData, DATA_URL } from './services/dataLoader';
+import React, { useState } from 'react';
+import { HashRouter } from 'react-router-dom';
+// import { CharacterData, DATA_URL } from './services/dataLoader';
 import { LocalStorageService, LS_KEYS } from './services/localStorage';
-// import { Header } from './components/header';
-// import { ContentList } from './components/content-list';
-// import { PersonCart } from './components/pages/person-cart';
 import { CharactersProvider } from './hooks/useCharacters';
 import AppRoutes from './routes/router';
 
@@ -13,16 +10,14 @@ import './App.css';
 function App() {
 
   const [characterName, setCharacterName] = useState(LocalStorageService.get(LS_KEYS.NAME) || '');
-  // useEffect(() => {
-  //   CharacterData.set(DATA_URL.PATH);
-  //   // setCharacters(LocalStorageService.get(LS_KEYS.CHARACTERS));
-  // }, []);
 
   return (
     <div className="App">
-      <CharactersProvider value={{characterName, setCharacterName}}>
-        <AppRoutes />
-      </CharactersProvider>
+      <HashRouter basename='/'>
+        <CharactersProvider value={{characterName, setCharacterName}}>
+          <AppRoutes />
+        </CharactersProvider>
+      </HashRouter>
     </div>
   );
 }
