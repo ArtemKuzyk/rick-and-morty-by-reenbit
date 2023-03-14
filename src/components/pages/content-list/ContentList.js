1806d584f862fd32a198de4b4e4fa05f3897aa31
ArtemKuzyk <artem.brar@gmail.com>
2023-03-14 15:58:50 +0200

    add div container for fb button

--- src/components/pages/content-list/ContentList.js
+++ src/components/pages/content-list/ContentList.js
@@ -53,10 +53,12 @@ export function ContentList(){
     return(
         <>
             <Header />
-            <div id="fb-root"></div>
-            <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v16.0&appId=2268895256627157&autoLogAppEvents=1" nonce="tOdeewMJ"></script>
-            {/* <div className="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false"></div> */}
-            <div className="fb-login-button" data-width="300px" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="true"></div>
+            <div className='my-fb-container'>
+                <div id="fb-root"></div>
+                <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v16.0&appId=2268895256627157&autoLogAppEvents=1" nonce="tOdeewMJ"></script>
+                {/* <div className="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false"></div> */}
+                <div className="fb-login-button" data-width="300px" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="true"></div>
+            </div>
             <main>
                 {Array.isArray(characters)?
                 characters.map(el => {
